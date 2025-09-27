9737e07b8e9221f0fdc48e2604174a76a1d3af97
Denys Tersiantsev <gureyndormin@gmail.com>
2025-09-27 13:21:38 +0300

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MediaLibrary
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
