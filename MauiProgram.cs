3494d98f8485d8efb1af9bd4269e5041f95bd812
Denys Tersiantsev <gureyndormin@gmail.com>
2025-10-12 18:46:12 +0300

    retrieving files sample

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,27 +1,32 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using ManagedCode.Storage.FileSystem.Extensions;
+using ManagedCode.Storage.FileSystem.Options;
+using ManagedCode.Storage.VirtualFileSystem.Extensions;
+using Microsoft.Extensions.Logging;
 
-namespace MediaLibrary
+namespace MediaLibrary;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
-            builder.Services.AddMauiBlazorWebView();
-
+        builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddFileSystemStorageAsDefault(new FileSystemStorageOptions());
+        builder.Services.AddVirtualFileSystem();
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
